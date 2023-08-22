33cb0b98d3350dd7450431f5b432f2eb049e6210
Dan <pen22217@uvg.edu.gt>
2023-08-22 13:54:32 -0600

    Added Parallel Sections

--- ParallelFactorial.cpp
+++ ParallelFactorial.cpp
@@ -1,23 +1,41 @@
 // C++ program to find factorial of given number
 #include <iostream>
 using namespace std;
+#include <omp.h>
+
 // function to find factorial of given number
 unsigned int factorial(unsigned int n)
 {
-    if (n == 0){
-        return 1;
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            #pragma omp single
+            {
+                if (n == 0){
+                return 1;
+                }
+            }
+        }
+
+        #pragma omp section 
+        {
+        return n * factorial(n - 1);
+        }
     }
-    return n * factorial(n - 1);
 }
+
 // Driver code
-int main(){
+int main() {
     int num;
 
-    cout << "Ingrese un numero:";
+    cout << "Type a number:";
     cin >> num;
+    
+    int result = factorial(num);
 
     cout << "Factorial of "
-    << num << " is " << factorial(num) << endl;
+    << num << " is " << result << endl;
     return 0;
 }
 // Based on the code made by Shivi_Aggarwal
